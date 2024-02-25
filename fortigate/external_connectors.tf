bb8124ef82b3eaf321277712026c8018df161e67
robert-gaines <robert.gaines@hushmail.com>
2024-02-25 04:14:02 -0800

    More feeds

--- fortigate/external_connectors.tf
+++ fortigate/external_connectors.tf
@@ -1,7 +1,48 @@
 
 resource "fortios_system_externalresource" "proofpoint-emerging-threats" {
   name         = "proofpoint-emerging-threats"
-  refresh_rate = 5
+  refresh_rate = 15
   resource     = "https://rules.emergingthreats.net/fwrules/emerging-Block-IPs.txt"
   status       = "enable"
+  type         = "address"
+}
+
+resource "fortios_system_externalresource" "feodo-botnet-c2" {
+  name         = "feodo-botnet-c2"
+  refresh_rate = 15
+  resource     = "https://feodotracker.abuse.ch/downloads/ipblocklist_aggressive.txt"
+  status       = "enable"
+  type         = "address"
+}
+
+resource "fortios_system_externalresource" "honeypot-intruders" {
+  name         = "honeypot-intruders"
+  refresh_rate = 15
+  resource     = "https://robertwgaines.com/feed.txt"
+  status       = "enable"
+  type         = "address"
+}
+
+resource "fortios_system_externalresource" "abusech-urlhaus" {
+  name         = "abusech-urlhaus"
+  refresh_rate = 15
+  resource     = "https://urlhaus.abuse.ch/downloads/text_online/"
+  status       = "enable"
+  type         = "domain"
+}
+
+resource "fortios_system_externalresource" "firehol-blocklist" {
+  name         = "firehol-blocklist"
+  refresh_rate = 15
+  resource     = "https://raw.githubusercontent.com/ktsaou/blocklist-ipsets/master/firehol_level1.netset"
+  status       = "enable"
+  type         = "address"
+}
+
+resource "fortios_system_externalresource" "cymru-bogons" {
+  name         = "cymru-bogons"
+  refresh_rate = 15
+  resource     = "https://www.team-cymru.org/Services/Bogons/fullbogons-ipv4.txt"
+  status       = "enable"
+  type         = "address"
 }
