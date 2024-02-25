40ed60776c201ed766140506c073b10621ad81a9
robert-gaines <robert.gaines@hushmail.com>
2024-02-24 22:06:30 -0800

    routine

--- fortigate/policies_vip_ingress.tf
+++ fortigate/policies_vip_ingress.tf
@@ -44,6 +44,11 @@ resource "fortios_firewall_policy" "deny-threat-objects-to-webserver" {
   schedule           = "always"
   nat                = "disable"
   internet_service   = "enable"
+  block_notification = "enable"
+
+  service {
+    name = "all"
+  }
 
   dstaddr {
     name = "webserver-vip-group"
@@ -53,16 +58,64 @@ resource "fortios_firewall_policy" "deny-threat-objects-to-webserver" {
     name = "webservers"
   }
 
-  service {
-    name = "HTTP"
+  internet_service_name {
+    name = "Botnet-C&C.Server"
   }
 
-  service {
-    name = "HTTPS"
+  internet_service_name {
+    name = "BinaryEdge-Scanner"
   }
 
-  srcaddr {
-    name = "PermittedCountries"
+  internet_service_name {
+    name = "Censys-Scanner"
+  }
+
+  internet_service_name {
+    name = "CriminalIP-Scanner"
+  }
+
+  internet_service_name {
+    name = "Cyber.Casa-Scanner"
+  }
+
+  internet_service_name {
+    name = "Internet.Census.Group-Scanner"
+  }
+
+  internet_service_name {
+    name = "InternetTTL-Scanner"
+  }
+
+  internet_service_name {
+    name = "LeakIX-Scanner"
+  }
+
+  internet_service_name {
+    name = "NetScout-Scanner"
+  }
+
+  internet_service_name {
+    name = "Recyber-Scanner"
+  }
+
+  internet_service_name {
+    name = "Shadowserver-Scanner"
+  }
+
+  internet_service_name {
+    name = "Shodan-Scanner"
+  }
+
+  internet_service_name {
+    name = "Stretchoid-Scanner"
+  }
+
+  internet_service_name {
+    name = "Tenable-Tenable.io.CloudScanner"
+  }
+
+  internet_service_name {
+    name = "UK.NCSC-Scanner"
   }
 
   srcintf {
