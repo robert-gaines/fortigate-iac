15de4e10e3157eef15607d9cfae235af5372a837
Robert Gaines <robert.gaines@hushmail.com>
2024-03-02 11:28:12 -0800

    Trying remote variable reference

--- fortigate/main.tf
+++ fortigate/main.tf
@@ -6,17 +6,17 @@ terraform {
     }
 
     backend "http" {
-        address = "$TF_BACKEND_ADDRESS"
-        lock_address = "$TF_BACKEND_LOCK_ADDRESS"
-        unlock_address = "$TF_BACKEND_UNLOCK_ADDRESS"
-        username = "$TF_BACKEND_USERNAME"
-        password = "$TF_BACKEND_PASSWORD"
+        address = var.TF_BACKEND_ADDRESS
+        lock_address = var.TF_BACKEND_LOCK_ADDRESS
+        unlock_address = var.TF_BACKEND_UNLOCK_ADDRESS
+        username = var.TF_BACKEND_USERNAME
+        password = var.TF_BACKEND_PASSWORD
   }
 }
 
 provider fortios {
     hostname = "192.168.1.99"
-    token = "$TF_FORTIOS_TOKEN"
+    token = var.TF_FORTIOS_TOKEN
     insecure = "true"
 }
 
