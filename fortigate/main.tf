c570776d3d99676c1cf4ae4f93c5231834ed79c3
Robert Gaines <robert.gaines@hushmail.com>
2024-03-02 13:27:21 -0800

    Test

--- fortigate/main.tf
+++ fortigate/main.tf
@@ -18,24 +18,24 @@ resource "fortios_system_global" "res-phy-prd-fwp-global" {
     timezone = "04"
     dst      = "enable"
     language = "english"
-    admintimeout          = 20
-    admin_console_timeout = 20
-    admin_port            = 8000
-    admin_sport           = 8443
-    admin_ssh_port        = 2222 
-    admin_https_redirect  = "enable" 
-    gui_certificates      = true
-    strong_crypto         = "enable"
-    lldp_transmission     = "enable"
-    asymroute             = "disable"
-    pre_login_banner      = "enable"
-    #
-    vdom_mode             = "no-vdom"
-    cfg_save              = "automatic"
-    #
-    switch_controller     = "enable"
-    #
-    sslvpn_web_mode       = "disable" 
+    # admintimeout          = 20
+    # admin_console_timeout = 20
+    # admin_port            = 8000
+    # admin_sport           = 8443
+    # admin_ssh_port        = 2222 
+    # admin_https_redirect  = "enable" 
+    # gui_certificates      = true
+    # strong_crypto         = "enable"
+    # lldp_transmission     = "enable"
+    # asymroute             = "disable"
+    # pre_login_banner      = "enable"
+    # #
+    # vdom_mode             = "no-vdom"
+    # cfg_save              = "automatic"
+    # #
+    # switch_controller     = "enable"
+    # #
+    # sslvpn_web_mode       = "disable" 
 
 
 }
