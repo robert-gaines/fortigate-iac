e716234ecb1076d0bbbf46243db47f80c0587a34
Robert Gaines <robert.gaines@hushmail.com>
2024-03-02 13:22:37 -0800

    FortiOS System Global Config

--- fortigate/main.tf
+++ fortigate/main.tf
@@ -8,11 +8,35 @@ terraform {
 
 provider "fortios" {
     hostname = "192.168.1.99"
-    token = var.FORTIOS_TOKEN
+    token    = var.FORTIOS_TOKEN
     insecure = "true"
 }
 
-resource "fortios_firewall_address" "res-phy-prd-fwp" {
-    name = "res-phy-prd-fwp"
+resource "fortios_system_global" "res-phy-prd-fwp" {
+    hostname = "res-phy-prd-fwp"
+    alias    = "FGT60ETK20049899"
+    timezone = "04"
+    dst      = "enable"
+    language = "english"
+    admintimeout          = 20
+    admin_console_timeout = 20
+    admin_port            = 8000
+    admin_sport           = 8443
+    admin_ssh_port        = 2222 
+    admin_https_redirect  = "enable" 
+    gui_certificates      = true
+    strong_crypto         = "enable"
+    lldp_transmission     = "enable"
+    asymroute             = "disable"
+    pre_login_banner      = "enable"
+    #
+    vdom_mode             = "no-vdom"
+    cfg_save              = "automatic"
+    #
+    switch_controller     = "enable"
+    #
+    sslvpn_web_mode       = "disable" 
+
+
 }
 
