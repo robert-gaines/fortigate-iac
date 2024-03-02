a0d974b73dadefa61c1517dd7703d197cbc5a819
Robert Gaines <robert.gaines@hushmail.com>
2024-03-02 13:29:18 -0800

    Routine

--- fortigate/main.tf
+++ fortigate/main.tf
@@ -15,28 +15,8 @@ provider "fortios" {
 resource "fortios_system_global" "res-phy-prd-fwp-global" {
     hostname = "res-phy-prd-fwp"
     alias    = "FGT60ETK20049899"
-    timezone = "04"
+    timezone = 04
     dst      = "enable"
     language = "english"
-    # admintimeout          = 20
-    # admin_console_timeout = 20
-    # admin_port            = 8000
-    # admin_sport           = 8443
-    # admin_ssh_port        = 2222 
-    # admin_https_redirect  = "enable" 
-    # gui_certificates      = true
-    # strong_crypto         = "enable"
-    # lldp_transmission     = "enable"
-    # asymroute             = "disable"
-    # pre_login_banner      = "enable"
-    # #
-    # vdom_mode             = "no-vdom"
-    # cfg_save              = "automatic"
-    # #
-    # switch_controller     = "enable"
-    # #
-    # sslvpn_web_mode       = "disable" 
-
-
 }
 
