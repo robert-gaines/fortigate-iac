267c543f07a70fb13e019e5e8bb98c566e396f1c
Robert Gaines <robert.gaines@hushmail.com>
2024-03-03 13:37:30 -0800

    routine

--- fortigate/sslinspection.tf
+++ fortigate/sslinspection.tf
@@ -1,8 +1,14 @@
 resource "fortios_firewall_sslsshprofile" "certificate-inspection-primary" {
-  name = "certificate-inspection-primary"
+  name    = "certificate-inspection-primary"
+  comment = "Primary SSL/TLS Inspection Profile"
 
   ssl {
-    inspect_all = "certificate-inspection"
+    inspect_all             = "certificate-inspection"
+    expired_server_cert     = "allow"
+    revoked_server_cert     = "block"
+    invalid_server_cert     = "block"
+    sni_server_cert_check   = "enable"
+    min_allowed_ssl_version = "tls-1.0"
   }
 
   https {
@@ -20,5 +26,6 @@ resource "fortios_firewall_sslsshprofile" "certificate-inspection-primary" {
   smtps {
     ports = 465
   }
+
 }
 
