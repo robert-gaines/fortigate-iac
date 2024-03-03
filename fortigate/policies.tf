e8b74f15a4e85798aae13625761c3b3995b0f41f
Robert Gaines <robert.gaines@hushmail.com>
2024-03-03 14:19:30 -0800

    routine

--- fortigate/policies.tf
+++ fortigate/policies.tf
@@ -41,3 +41,46 @@ resource "fortios_firewall_policy" "permit-workstations-to-wan" {
     name = "workstations"
   }
 }
+
+resource "fortios_firewall_policy" "permit-servers-to-wan" {
+  action             = "accept"
+  logtraffic         = "all"
+  name               = "permit-servers-to-wan"
+  schedule           = "always"
+  nat                = "enable"
+  utm_status         = "enable" 
+  inspection_mode    = "flow" 
+  av_profile         = "av-flow"
+  ips_sensor         = "ips-primary"  
+  webfilter_profile  = "webfilter-primary"
+  application_list   = "app-control-primary"
+  ssl_ssh_profile    = "certificate-inspection-primary"  
+
+  dstaddr {
+    name = "all"
+  }
+
+  dstintf {
+    name = "virtual-wan-link"
+  }
+
+  service {
+    name = "DNS"
+  }
+
+  service {
+    name = "HTTP"
+  }
+
+  service {
+    name = "HTTPS"
+  }
+
+  srcaddr {
+    name = "servers"
+  }
+
+  srcintf {
+    name = "servers"
+  }
+}
