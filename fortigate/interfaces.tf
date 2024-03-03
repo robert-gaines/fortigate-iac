969fdf0eb141679c33c250d58390ec0033150661
Robert Gaines <robert.gaines@hushmail.com>
2024-03-02 22:02:05 -0800

    routine

--- fortigate/interfaces.tf
+++ fortigate/interfaces.tf
@@ -1,11 +1,20 @@
 
 resource "fortios_system_interface" "dmz" {
   ip          = "10.10.10.1 255.255.255.0"
-  allowaccess = ""
   name        = "dmz"
   role        = "lan"
   type        = "physical"
   vdom        = "root"
   mode        = "static"
   status      = "down"
+}
+
+resource "fortios_system_interface" "dmz" {
+  ip          = "192.168.254.1 255.255.255.252"
+  name        = "fortilink"
+  role        = "fortilink"
+  type        = "physical"
+  vdom        = "root"
+  mode        = "static"
+  status      = "up"
 } 
