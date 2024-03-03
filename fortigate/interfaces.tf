f87b5c166ef99e1e06a968072dd3e130208736ca
Robert Gaines <robert.gaines@hushmail.com>
2024-03-02 21:59:11 -0800

    routine

--- fortigate/interfaces.tf
+++ fortigate/interfaces.tf
@@ -1,11 +1,11 @@
 
 resource "fortios_system_interface" "dmz" {
-  ip          = "172.28.255.253 255.255.255.255"
-  allowaccess = "ping https ssh snmp http telnet fgfm radius-acct probe-response fabric ftm speed-test"
+  ip          = "10.10.10.1 255.255.255.0"
+  allowaccess = ""
   name        = "dmz"
   role        = "lan"
   type        = "physical"
   vdom        = "root"
   mode        = "static"
-  status      = "up"
+  status      = "down"
 } 
