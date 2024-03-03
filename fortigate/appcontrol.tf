273c593bc105d18f2423b9cb7ed23ba56d658b08
Robert Gaines <robert.gaines@hushmail.com>
2024-03-03 12:17:43 -0800

    routine

--- fortigate/appcontrol.tf
+++ fortigate/appcontrol.tf
@@ -1,23 +1,25 @@
 
-resource "fortios_application_group" "app-control-primary" {
-  comment = "Primary Application Control Profile"
-  name    = "app-ctrl-primary"
-  type    = "category"
-  category {
-    id = 2
-  }
-}
 
 
-resource "fortios_application_list" "app-list-primary" {
+resource "fortios_application_list" "app-control-primary" {
   app_replacemsg             = "enable"
   deep_app_inspection        = "enable"
   enforce_default_app_port   = "disable"
   extended_log               = "disable"
-  name                       = "app-list-primary"
+  name                       = "app-control-primary"
+  comment                    = "Primary Application Control Profile"
   options                    = "allow-dns"
   other_application_action   = "pass"
   other_application_log      = "disable"
   unknown_application_action = "pass"
   unknown_application_log    = "disable"
+  #
+  p2p_black_list             = "skype,edonkey,bittorrent"
+  p2p_block_list             = "skype,edonkey,bittorrent"
+  #
+  entries {
+    id = "2,6,7,8"
+    action = "block"
+  }  
+
 }
