62240f54b93731d0877adfbecbe976df1e8db61a
Robert Gaines <robert.gaines@hushmail.com>
2024-03-03 10:43:13 -0800

    routine

--- fortigate/main.tf
+++ fortigate/main.tf
@@ -19,15 +19,7 @@ resource "fortios_system_global" "res-phy-prd-fwp-global" {
 resource "fortios_ips_global" "res-phy-prd-fwpglobal-ips" {
   anomaly_mode           = "continuous"
   database               = "regular"
-  deep_app_insp_db_limit = 0
-  deep_app_insp_timeout  = 0
-  engine_count           = 0
-  exclude_signatures     = "industrial"
   fail_open              = "disable"
-  intelligent_mode       = "enable"
-  session_limit_mode     = "heuristic"
-  socket_size            = 0
-  sync_session_ttl       = "enable"
-  traffic_submit         = "disable"
+
 }
 
