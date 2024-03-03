458c73439245f749578b69b245265f6981ce109b
Robert Gaines <robert.gaines@hushmail.com>
2024-03-03 10:04:51 -0800

    routine

--- fortigate/antivirus.tf
+++ fortigate/antivirus.tf
@@ -1,15 +1,8 @@
 
 resource "fortios_antivirus_profile" "av-flow" {
-  analytics_bl_filetype = 0
-  analytics_db          = "disable"
-  analytics_max_upload  = 10
-  analytics_wl_filetype = 0
   av_block_log          = "enable"
   av_virus_log          = "enable"
-  extended_log          = "disable"
-  ftgd_analytics        = "disable"
   inspection_mode       = "flow-based"
-  mobile_malware_db     = "enable"
-  name                  = "1"
-  scan_mode             = "quick"
+  name                  = "av-flow"
+  scan_mode             = "default"
 }
