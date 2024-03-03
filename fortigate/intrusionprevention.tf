abaf1856b81ec0ef3aae75f7b872ccb3fc9ad2eb
Robert Gaines <robert.gaines@hushmail.com>
2024-03-03 11:37:11 -0800

    routine

--- fortigate/intrusionprevention.tf
+++ fortigate/intrusionprevention.tf
@@ -6,41 +6,30 @@ resource "fortios_ips_sensor" "ips-primary" {
   scan_botnet_connections = "block"
   
   entries {
-    action = "block"
-    log = "enable"
-    log_packet = "enable"
+    action             = "block"
+    log                = "enable"
+    log_packet         = "enable"
     log_attack_context = "enable"
-    default_status = "all"
-    severity = "critical"
+    default_status     = "all"
+    severity           = "critical"
   }
 
    entries {
-    action = "block"
-    log = "enable"
-    log_packet = "enable"
+    action             = "block"
+    log                = "enable"
+    log_packet         = "enable"
     log_attack_context = "enable"
-    default_status = "all"
-    severity = "high"
+    default_status     = "all"
+    severity           = "high"
   }
 
-#   filter {
-#     severity = "low"
-#     action = "block"
-#   }
-
-#   filter {
-#     severity = "medium"
-#     action = "block"
-#   }
-
-#   filter {
-#     severity = "high"
-#     action = "block"
-#   }
-
-  filter {
-    severity = "critical"
-    action = "block"
+   entries {
+    action             = "block"
+    log                = "enable"
+    log_packet         = "enable"
+    log_attack_context = "enable"
+    default_status     = "all"
+    severity           = "medium"
   }
 
 }
