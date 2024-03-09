07cfaaba1147ef8c8b2548ee1b36b6466c89caf9
Robert Gaines <robert.gaines@hushmail.com>
2024-03-09 14:18:18 -0800

    routine

--- fortigate/syslog.tf
+++ fortigate/syslog.tf
@@ -22,3 +22,26 @@ resource "fortios_logsyslogd_filter" "syslog-filter-one" {
   sniffer_traffic   = "enable"
   ssh               = "enable"
 }
+
+resource "fortios_logsyslogd_setting" "syslog-config-two" {
+    status        = "enable"
+    enc_algorithm = "disable"
+    mode          = "udp"
+    port          = "514"
+    server        = "10.128.10.8"
+    facility      =  "local7"
+    interface     = "servers"
+    source_ip     = "10.128.10.1"
+}
+
+resource "fortios_logsyslogd_filter" "syslog-filter-two" {
+  anomaly           = "enable"
+  dns               = "enable"
+  filter_type       = "include"
+  forward_traffic   = "enable"
+  local_traffic     = "enable"
+  multicast_traffic = "enable"
+  severity          = "information"
+  sniffer_traffic   = "enable"
+  ssh               = "enable"
+}
