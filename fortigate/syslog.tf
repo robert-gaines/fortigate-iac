debe2e08c8e02a6e77075f18eec0a04a64f3232d
Robert Gaines <robert.gaines@hushmail.com>
2024-03-09 13:48:18 -0800

    routine

--- fortigate/syslog.tf
+++ fortigate/syslog.tf
@@ -4,7 +4,8 @@ resource "fortios_logsyslogd_setting" "syslog-config-one" {
     enc_algorithm = "disable"
     mode          = "udp"
     port          = "514"
-    facility      =  "local7"
+    server        = "10.128.10.5"
+    facility      =  "local0"
     interface     = "servers"
     source_ip     = "10.128.10.1"
 }
