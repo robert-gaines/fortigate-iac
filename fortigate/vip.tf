d5b867ec3a2536e39a199661db594f97b99b5531
Robert Gaines <robert.gaines@hushmail.com>
2024-03-10 09:55:27 -0700

    VIP PT 2

--- fortigate/vip.tf
+++ fortigate/vip.tf
@@ -11,6 +11,20 @@ resource "fortios_firewall_vip" "webserver-vip-https" {
   service {
     name = "HTTPS"
   }
+}
 
+resource "fortios_firewall_vip" "webserver-vip-http" {
+  name             = "res-vrt-prd-wsr:80"
+  extintf          = "wan1" 
+  extip            = "192.168.255.1"
+  extport          = "443" 
+  color            = 21
+  comment          = "Web Server - HTTP" 
+  mappedip {
+    range = "10.128.40.2"
+  }
+  service {
+    name = "HTTP"
+  }
 }
 
