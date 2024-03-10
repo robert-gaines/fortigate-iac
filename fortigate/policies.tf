aa4927651a86f88a40074959024cc28ea70e62ba
Robert Gaines <robert.gaines@hushmail.com>
2024-03-10 10:04:00 -0700

    VIP Ingress Policy

--- fortigate/policies.tf
+++ fortigate/policies.tf
@@ -272,3 +272,40 @@ resource "fortios_firewall_policy" "permit-workstations-to-admin" {
     name = "workstations"
   }
 }
+
+resource "fortios_firewall_policy" "permit-wan-to-webserver" {
+  action             = "accept"
+  logtraffic         = "all"
+  name               = "permit-wan-to-webserver"
+  schedule           = "always"
+  nat                = "disable"
+  utm_status         = "enable" 
+  inspection_mode    = "flow" 
+  av_profile         = "av-flow"
+  ips_sensor         = "ips-primary"  
+  ssl_ssh_profile    = "certificate-inspection-primary"  
+
+  dstaddr {
+    name = "webserver-vip-group"
+  }
+
+  dstintf {
+    name = "webservers"
+  }
+
+  service {
+    name = "HTTP"
+  }
+
+  service {
+    name = "HTTPS"
+  }
+
+  srcaddr {
+    name = "PermittedCountries"
+  }
+
+  srcintf {
+    name = "virtual-wan-link"
+  }
+}
