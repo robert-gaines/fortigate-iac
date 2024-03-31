db9b3194e4e1054229ddb26955382fec3882163f
robert-gaines <robert.gaines@hushmail.com>
2024-03-31 15:50:45 -0700

    More custom service additions

--- fortigate/service_groups.tf
+++ fortigate/service_groups.tf
@@ -6,6 +6,7 @@ resource "fortios_firewallservice_group" "identity-management-services" {
     member { name  = "HTTP" }
     member { name  = "HTTPS" }
     member { name  = "LDAP" }
+    member { name  = "LDAPS" }
     member { name  = "LDAP_UDP" }
     member { name  = "KERBEROS" }
     
