4c43e2f39fabeecb9f0787b60b88e8b722310840
robert-gaines <robert.gaines@hushmail.com>
2024-03-31 15:38:29 -0700

    Adding identity management services

---
+++ fortigate/service_groups.tf
@@ -0,0 +1,12 @@
+resource "fortios_firewallservice_group" "identity-management-services" {
+
+    color = 0
+    name  = "identity-management-services"
+
+    member { name  = "HTTP" }
+    member { name  = "HTTPS" }
+    member { name  = "LDAP" }
+    member { name  = "LDAPS" }
+    member { name  = "KERBEROS" }
+    
+}
--- fortigate/waf.tf
+++ fortigate/waf.tf
@@ -1,29 +1,6 @@
 resource "fortios_waf_profile" "waf-primary" {
-  extended_log = "disable"
+  extended_log = "enable"
   external     = "enable"
   name         = "waf-primary"
-  
-  method {
-    status                  = "enable"
-    log                     = "enable"
-    default_allowed_methods = "get"
-  }
-
-  signature {
-    main_class {
-      id       = "50000000"
-      log      = "enable"
-      severity = "medium"
-    }
-  }
-
-  constraint {
-    method {
-      action   = "allow"
-      log      = "enable"
-      status   = "enable"
-      severity = "low"
-    }
-  }
 }
 
