5eb1b7e5b24120d89278fa60ec270c1213802140
robert-gaines <robert.gaines@hushmail.com>
2024-03-31 16:58:00 -0700

    Adding webserver egress policy

---
+++ fortigate/policies_webservers_egress.tf
@@ -0,0 +1,48 @@
+
+resource "fortios_firewall_policy" "permit-webserver-to-wan" {
+  action             = "accept"
+  logtraffic         = "all"
+  name               = "permit-webserver-to-wan"
+  schedule           = "always"
+  nat                = "disable"
+  utm_status         = "enable" 
+  inspection_mode    = "flow" 
+  av_profile         = "av-flow"
+  ips_sensor         = "ips-primary"
+  webfilter_profile  = "webfilter-primary"
+  application_list   = "app-control-primary"  
+  ssl_ssh_profile    = "certificate-inspection-primary"  
+
+  dstaddr {
+    name = "PermittedCountries"
+  }
+
+  dstintf {
+    name = "virtual-wan-link"
+  }
+
+  service {
+    name = "HTTP"
+  }
+
+  service {
+    name = "HTTPS"
+  }
+
+  service {
+    name = "DNS"
+  }
+
+  service {
+    name = "NTP"
+  }
+
+  srcaddr {
+    name = "webservers"
+  }
+
+  srcintf {
+    name = "webservers"
+  }
+  
+}
