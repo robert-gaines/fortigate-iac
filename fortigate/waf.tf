a4f49159b091171f865b35c358217d4d5791f0ea
robert-gaines <robert.gaines@hushmail.com>
2024-03-31 15:02:47 -0700

    WAF Signatures - Maybe?

--- fortigate/waf.tf
+++ fortigate/waf.tf
@@ -3,5 +3,13 @@ resource "fortios_waf_profile" "waf-primary" {
   external     = "enable"
   name         = "waf-primary"
   
-  
+  method {
+    status                  = "enable"
+    log                     = "enable"
+    default_allowed_methods = "get"
+  }
+}
+
+resource "fortios_waf_signature" "waf-primary-signatures" {
+  fosid = "50000000"
 }
