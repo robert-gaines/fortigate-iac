c7f80aa6c4f4f2c49e67f873b5b0e3478398de4b
robert-gaines <robert.gaines@hushmail.com>
2024-04-07 12:37:10 -0700

    Adding WLAN DHCP server definitions

--- fortigate/dhcp.tf
+++ fortigate/dhcp.tf
@@ -12,3 +12,51 @@ resource "fortios_systemdhcp_server" "v70-wireless-dhcp" {
     start_ip = "10.128.70.2"
   }
 }
+
+resource "fortios_systemdhcp_server" "wireless-iot-dhcp" {
+  # Workstations WLAN DHCP Server
+  dns_service = "default"
+  ntp_service = "default"
+  fosid       = 1
+  interface   = "res-nwk-sid-wkn"
+  netmask     = "255.255.255.240"
+  status      = "enable"
+
+  ip_range {
+    end_ip   = "10.128.80.2"
+    id       = 1
+    start_ip = "10.128.80.14"
+  }
+}
+
+resource "fortios_systemdhcp_server" "wireless-iot-dhcp" {
+  # IoT WLAN DHCP Server
+  dns_service = "default"
+  ntp_service = "default"
+  fosid       = 1
+  interface   = "res-nwk-sid-iot"
+  netmask     = "255.255.255.192"
+  status      = "enable"
+
+  ip_range {
+    end_ip   = "10.128.90.2"
+    id       = 1
+    start_ip = "10.128.90.62"
+  }
+}
+
+resource "fortios_systemdhcp_server" "wireless-gpn-dhcp" {
+  # General Purpose WLAN DHCP Server
+  dns_service = "default"
+  ntp_service = "default"
+  fosid       = 1
+  interface   = "res-nwk-sid-gpn"
+  netmask     = "255.255.255.240"
+  status      = "enable"
+
+  ip_range {
+    end_ip   = "10.128.100.2"
+    id       = 1
+    start_ip = "10.128.100.14"
+  }
+}
