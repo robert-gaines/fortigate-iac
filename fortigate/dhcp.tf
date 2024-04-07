f5f35e0fe6be06979b59de88b9e1521401e7a747
robert-gaines <robert.gaines@hushmail.com>
2024-04-07 13:12:05 -0700

    Fixing default gateways

--- fortigate/dhcp.tf
+++ fortigate/dhcp.tf
@@ -14,12 +14,12 @@ resource "fortios_systemdhcp_server" "v70-wireless-dhcp" {
 
 resource "fortios_systemdhcp_server" "wireless-wkn-dhcp" {
   # Workstations WLAN DHCP Server
-  dns_service = "default"
-  ntp_service = "default"
-  interface   = "res-nwk-sid-wkn"
-  netmask     = "255.255.255.240"
-  status      = "enable"
-  default_gateway = "default"
+  dns_service     = "default"
+  ntp_service     = "default"
+  interface       = "res-nwk-sid-wkn"
+  netmask         = "255.255.255.240"
+  status          = "enable"
+  default_gateway = "10.128.80.1"
 
   ip_range {
     end_ip   = "10.128.80.14"
@@ -30,11 +30,12 @@ resource "fortios_systemdhcp_server" "wireless-wkn-dhcp" {
 
 resource "fortios_systemdhcp_server" "wireless-iot-dhcp" {
   # IoT WLAN DHCP Server
-  dns_service = "default"
-  ntp_service = "default"
-  interface   = "res-nwk-sid-iot"
-  netmask     = "255.255.255.192"
-  status      = "enable"
+  dns_service     = "default"
+  ntp_service     = "default"
+  interface       = "res-nwk-sid-iot"
+  netmask         = "255.255.255.192"
+  status          = "enable"
+  default_gateway = "10.128.90.1"
 
   ip_range {
     end_ip   = "10.128.90.62"
@@ -45,11 +46,12 @@ resource "fortios_systemdhcp_server" "wireless-iot-dhcp" {
 
 resource "fortios_systemdhcp_server" "wireless-gpn-dhcp" {
   # General Purpose WLAN DHCP Server
-  dns_service = "default"
-  ntp_service = "default"
-  interface   = "res-nwk-sid-gpn"
-  netmask     = "255.255.255.240"
-  status      = "enable"
+  dns_service     = "default"
+  ntp_service     = "default"
+  interface       = "res-nwk-sid-gpn"
+  netmask         = "255.255.255.240"
+  status          = "enable"
+  default_gateway = "10.128.100.1"
 
   ip_range {
     end_ip   = "10.128.100.14"
