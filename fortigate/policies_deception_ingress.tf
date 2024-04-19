4887623325e41b6a9793d10991d39c11ab232305
robert-gaines <robert.gaines@hushmail.com>
2024-04-18 20:32:38 -0700

    routine

--- fortigate/policies_deception_ingress.tf
+++ fortigate/policies_deception_ingress.tf
@@ -30,3 +30,36 @@ resource "fortios_firewall_policy" "permit-administration-to-deception" {
     name = "administration"
   }
 }
+
+resource "fortios_firewall_policy" "permit-prometheus-to-deception" {
+  action             = "accept"
+  logtraffic         = "all"
+  name               = "permit-prometheus-to-deception"
+  schedule           = "always"
+  nat                = "disable"
+  utm_status         = "enable" 
+  inspection_mode    = "flow" 
+  av_profile         = "av-flow"
+  ips_sensor         = "ips-primary"  
+  ssl_ssh_profile    = "certificate-inspection-primary"  
+
+  dstaddr {
+    name = "deception"
+  }
+
+  dstintf {
+    name = "deception"
+  }
+
+  service {
+    name = "prometheus-exporter"
+  }
+
+  srcaddr {
+    name = "sres-phy-prd-rpi-3"
+  }
+
+  srcintf {
+    name = "servers"
+  }
+}
