bf13d3847a7fe95c733780b48a1ac9df62ca2126
robert-gaines <robert.gaines@hushmail.com>
2024-04-21 13:17:51 -0700

    Prometheus webserver ingress policy

---
+++ fortigate/policies_webservers_ingress.tf
@@ -0,0 +1,34 @@
+
+resource "fortios_firewall_policy" "permit-prometheus-to-webserver" {
+  action             = "accept"
+  logtraffic         = "all"
+  name               = "permit-prometheus-to-webserver"
+  schedule           = "always"
+  nat                = "enable"
+  utm_status         = "enable" 
+  inspection_mode    = "flow" 
+  av_profile         = "av-flow"
+  ips_sensor         = "ips-primary"  
+  ssl_ssh_profile    = "certificate-inspection-primary"  
+
+  dstaddr {
+    name = "res-vrt-prd-wsr"
+  }
+
+  dstintf {
+    name = "webservers"
+  }
+
+  service {
+    name = "prometheus-exporter"
+  }
+
+  srcaddr {
+    name = "res-phy-prd-rpi-3"
+  }
+
+  srcintf {
+    name = "servers"
+  }
+
+}
