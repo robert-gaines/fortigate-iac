dda5c94d855a1fcb985d299b1b940787a31a9a9e
robert-gaines <robert.gaines@hushmail.com>
2024-05-22 12:28:30 -0700

    Routine

--- fortigate/usergroups.tf
+++
@@ -1,8 +0,0 @@
-resource "fortios_user_group" "vpn-users" {
-  group_type         = "firewall"
-  name               = "vpn-users"
-  match {
-    server_name = "res-vrt-prd-dss-ldaps"
-    group_name  = "Residence.VPN.Users"
-  }
-}
