ad900f6d3e27aecd5d100ee582891860029e2083
robert-gaines <robert.gaines@hushmail.com>
2024-05-22 12:21:37 -0700

    user group addition

--- fortigate/usergroups.tf
+++ fortigate/usergroups.tf
@@ -1,6 +1,6 @@
-resource "fortios_user_group" "internal-ldap-users" {
-  group_type         = "rsso"
-  name               = "residence-vpn-users"
+resource "fortios_user_group" "vpn-users" {
+  group_type         = "firewall"
+  name               = "vpn-users"
   match {
     server_name = "res-vrt-prd-dss-ldaps"
     group_name  = "Residence.VPN.Users"
