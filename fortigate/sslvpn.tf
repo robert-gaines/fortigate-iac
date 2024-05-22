f19a5f5c55432f7519984d8d65793b64947139a1
robert-gaines <robert.gaines@hushmail.com>
2024-05-22 08:19:25 -0700

    Attempting SSLVPn config

---
+++ fortigate/sslvpn.tf
@@ -0,0 +1,22 @@
+resource "fortios_vpnssl_settings" "ra-vpn-config" {
+  status                = "enable"
+  login_attempt_limit   = 3
+  login_block_time      = 60
+  login_timeout         = 0
+  auth_timeout          = 0
+  idle_timeout          = 0
+  port                  = 4343
+  servercert            = "self-sign"
+  tlsv1_3               = "enable"
+  ssl_min_proto_ver     = "tls1-3"
+  ciphersuite           = "TLS-AES-256-GCM-SHA384"
+  force_two_factor_auth = "enable"
+  ssl_client_renegotiation = "enable"
+  algorithm = "high"
+  dtls_tunnel = "enable"
+  dtls_min_proto_ver = "dtls1-2"
+
+  source_address {
+    name = "PermittedCountries"
+  }
+}
