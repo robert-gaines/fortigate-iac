baa45b6a38da0644644fd85c7a71b8288d2b5eac
robert-gaines <robert.gaines@hushmail.com>
2024-06-05 09:43:31 -0700

    Adding Honeypot VIP Group

--- fortigate/vip_group.tf
+++ fortigate/vip_group.tf
@@ -12,3 +12,26 @@ resource "fortios_firewall_vipgrp" "webserver-vip-group" {
   }
 
 }
+
+resource "fortios_firewall_vipgrp" "honeypot-vip-group" {
+  color     = 11
+  interface = "wan1"
+  name      = "honeypot-vip-group"
+
+  member {
+    name = fortios_firewall_vip.honeypot-vip-conpot-bacnet.name
+  }
+
+  member {
+    name = fortios_firewall_vip.honeypot-vip-conpot-ethernetip.name
+  }
+
+  member {
+    name = fortios_firewall_vip.honeypot-vip-conpot-modbus.name
+  }
+
+  member {
+    name = fortios_firewall_vip.honeypot-vip-conpot-plc.name
+  }
+
+}
