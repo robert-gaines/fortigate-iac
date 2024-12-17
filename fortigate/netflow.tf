0152b00bb5ba00bba5cc47589847bef4816d666d
robert-gaines <robert.gaines@protonmail.com>
2024-12-16 20:47:12 -0800

    Reconfigure netflow collector

--- fortigate/netflow.tf
+++ fortigate/netflow.tf
@@ -1,10 +1,15 @@
 
 resource "fortios_system_netflow" "netflow-primary" {
     active_flow_timeout = 60
-    collector_ip = "10.128.10.7"
-    collector_port = 2055
     inactive_flow_timeout = 15
-    source_ip = "10.128.10.1"
     template_tx_counter = 20
     template_tx_timeout = 60
+
+    collectors {
+        id = "1"
+        collector_ip = "10.128.10.7"
+        collector_port = 2055
+        interface_select_method = "auto"
+        interface="servers"
+    }
 }
