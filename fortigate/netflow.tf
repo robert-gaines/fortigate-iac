c023f33b9cd000714ab17f372f7f100a2d4c99aa
robert-gaines <robert.gaines@protonmail.com>
2024-12-16 20:09:15 -0800

    Netflow 2nd attempt

--- fortigate/netflow.tf
+++ fortigate/netflow.tf
@@ -3,7 +3,6 @@ resource "fortios_system_netflow" "netflow-primary" {
     active_flow_timeout = 30
     collector_ip = "10.128.10.7"
     collector_port = 2055
-    interface = "servers"
     inactive_flow_timeout = 15
     source_ip = "10.128.10.1"
     template_tx_counter = 20
