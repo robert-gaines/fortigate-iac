53d50e97a98e1ed90394f4aff4a5606aea58f3f6
robert-gaines <robert.gaines@protonmail.com>
2025-01-02 14:55:35 -0800

    Updating VIPs

--- fortigate/vip.tf
+++ fortigate/vip.tf
@@ -38,7 +38,7 @@ resource "fortios_firewall_vip" "honeypot-vip-conpot-bacnet" {
   color            = 11
   comment          = "Honeypot - Conpot - BACNET" 
   mappedip {
-    range = "10.128.60.13"
+    range = "10.128.60.15"
   }
   service {
     name = "BACNET"
@@ -53,7 +53,7 @@ resource "fortios_firewall_vip" "honeypot-vip-conpot-ethernetip" {
   color            = 11
   comment          = "Honeypot - Conpot - ETHERNETIP" 
   mappedip {
-    range = "10.128.60.14"
+    range = "10.128.60.16"
   }
   service {
     name = "ETHERNETIP"
@@ -68,7 +68,7 @@ resource "fortios_firewall_vip" "honeypot-vip-conpot-modbus" {
   color            = 11
   comment          = "Honeypot - Conpot - MODBUS" 
   mappedip {
-    range = "10.128.60.11"
+    range = "10.128.60.13"
   }
   service {
     name = "MODBUS"
@@ -79,14 +79,44 @@ resource "fortios_firewall_vip" "honeypot-vip-conpot-plc" {
   name             = "conpot-plc:102"
   extintf          = "wan1" 
   extip            = "192.168.255.1"
-  extport          = "502" 
+  extport          = "102" 
   color            = 11
   comment          = "Honeypot - Conpot - S7-PLC" 
   mappedip {
-    range = "10.128.60.15"
+    range = "10.128.60.17"
   }
   service {
     name = "S7-PLC"
+  }
+}
+
+resource "fortios_firewall_vip" "honeypot-vip-cowrie-ssh" {
+  name             = "cowrie-ssh:22"
+  extintf          = "wan1" 
+  extip            = "192.168.255.1"
+  extport          = "22" 
+  color            = 11
+  comment          = "Honeypot - Cowrie - SSH" 
+  mappedip {
+    range = "10.128.60.11"
+  }
+  service {
+    name = "SSH"
+  }
+}
+
+resource "fortios_firewall_vip" "honeypot-vip-dionaea-smb" {
+  name             = "dionea-smb:445"
+  extintf          = "wan1" 
+  extip            = "192.168.255.1"
+  extport          = "445" 
+  color            = 11
+  comment          = "Honeypot - Dionaea - SMB" 
+  mappedip {
+    range = "10.128.60.8"
+  }
+  service {
+    name = "SMB"
   }
 }
 
